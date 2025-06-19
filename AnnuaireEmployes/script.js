dc768c1202debe4befc3dad4ae32748b8bb50177
Carnelle Enga <carnelleenga@gmail.com>
2025-06-19 17:17:20 +0100

    ajout de modifications

--- AnnuaireEmployes/script.js
+++ AnnuaireEmployes/script.js
@@ -8,10 +8,11 @@ function saveToLocalStorage() {
   localStorage.setItem("employees", JSON.stringify(employees));
 }
 
-// fonction pour lister les employés sur le page
+// fonction pour lister les employés sur la page
 function listEmployes() {
   list.innerHTML = "";
   employees.forEach((emp, index) => {
+    // Création d'un élément li pour chaque employé
     const li = document.createElement("li");
     li.innerHTML = `
       <div>
@@ -21,6 +22,7 @@ function listEmployes() {
       </div>
       <button onclick="deleteEmploye(${index})">Supprimer</button>
     `;
+    // Ajout de l'élément li à la liste
     list.appendChild(li);
   });
 }
@@ -28,8 +30,12 @@ function listEmployes() {
 // fonction pour supprimmer un employé
 function deleteEmploye(index) {
     const emp = employees[index];
+    // Confirmation avant la suppression
     if (confirm(`Voulez-vous vraiment supprimer  l'employé ${emp.firstName} ${emp.lastName} ?`)) {
+        // Suppression de l'employé de la liste
         employees.splice(index, 1);
+        // Mise à jour du localStorage et de l'affichage
+        alert(`L'employé ${emp.firstName} ${emp.lastName} a été supprimé.`);
         saveToLocalStorage();
         listEmployes();
     }
@@ -38,17 +44,17 @@ function deleteEmploye(index) {
 // Gestion de l'envoi du formulaire : valide les champs, ajoute l'employé à la liste, met à jour le localStorage et réinitialise le formulaire.
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+    // Récupération des valeurs des champs du formulaire
   const lastName = document.getElementById("lastName").value.trim();
   const firstName = document.getElementById("firstName").value.trim();
   const email = document.getElementById("email").value.trim();
   const poste = document.getElementById("poste").value.trim();
-
+    // Validation des champs
   if (!lastName || !firstName || !email || !poste || !validateEmail(email)) {
     alert("Veuillez remplir tous les champs correctement.");
     return;
   }
-
+  
   const newEmployee = { lastName, firstName, email, poste };
   employees.push(newEmployee);
   saveToLocalStorage();
