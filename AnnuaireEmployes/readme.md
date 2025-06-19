272d337e05525ca3628337578c89042bb150588c
Carnelle Enga <carnelleenga@gmail.com>
2025-06-19 16:34:27 +0100

    fonctionnalites faites .

---
+++ AnnuaireEmployes/readme.md
@@ -0,0 +1,42 @@
+# 📘 Annuaire  des Employés d'Orange
+
+## Réalisé par :
+**Nom** : ENGA CHONGWANG 
+**Prénom** : Carnelle
+
+---
+
+## Technologies utilisées :
+- HTML
+- CSS
+- JavaScript 
+
+---
+
+## Temps de développement :
+**Durée réellement passée** : ~3h30 (sur les 4h prévues)
+
+---
+
+## Instructions pour lancer le projet :
+1. Télécharger ou cloner le projet via le lien soumis.
+2. Ouvrir le fichier `index.html` avec un double-clic ou dans un navigateur
+
+---
+
+##  Fonctionnalités principales développées :
+- Formulaire d’ajout d’un employé (Nom, Prénom, Email, Fonction)
+- Validation des champs (email valide, champs obligatoires)
+- Ajout dynamique dans la liste sans rechargement de page
+- Bouton **Supprimer** pour chaque employé
+- Sauvegarde des données avec `localStorage` (persistance)
+- Interface responsive avec CSS simple
+- Affichage clair et lisible
+
+---
+
+##  Bonus réalisés :
+- Message de confirmation avec nom/prénom avant suppression
+
+
+---
---
+++ AnnuaireEmployes/script.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("employeForm");
+const list = document.getElementById("employeList");
+
+let employees = JSON.parse(localStorage.getItem("employees")) || [];
+
+function saveToLocalStorage() {
+  localStorage.setItem("employees", JSON.stringify(employees));
+}
+
+function listEmployes() {
+  list.innerHTML = "";
+  employees.forEach((emp, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <div>
+        <strong>${emp.lastName} ${emp.firstName}</strong><br>
+        ${emp.email} –<br>
+        ${emp.poste}
+      </div>
+      <button onclick="deleteEmploye(${index})">Supprimer</button>
+    `;
+    list.appendChild(li);
+  });
+}
+
+function deleteEmploye(index) {
+    const emp = employees[index];
+    if (confirm(`Voulez-vous vraiment supprimer  l'employé ${emp.firstName} ${emp.lastName} ?`)) {
+        employees.splice(index, 1);
+        saveToLocalStorage();
+        listEmployes();
+    }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const lastName = document.getElementById("lastName").value.trim();
+  const firstName = document.getElementById("firstName").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const poste = document.getElementById("poste").value.trim();
+
+  if (!lastName || !firstName || !email || !poste || !validateEmail(email)) {
+    alert("Veuillez remplir tous les champs correctement.");
+    return;
+  }
+
+  const newEmployee = { lastName, firstName, email, poste };
+  employees.push(newEmployee);
+  saveToLocalStorage();
+  listEmployes();
+
+  form.reset();
+});
+
+function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+// affichage initial de la liste des employés
+listEmployes();
